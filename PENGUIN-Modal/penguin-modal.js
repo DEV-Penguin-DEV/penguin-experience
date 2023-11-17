ebd83c2f0d55813a10cd09cf576469a66003966e
PENGUIN <kosvadmail@atomlc.com.ua>
2023-11-17 08:10:25 +0100

    Правки по penguin modal и смена названия для scss скриптов

--- PENGUIN-Modal/penguin-modal.js
+++ PENGUIN-Modal/penguin-modal.js
@@ -1,137 +1,137 @@
 import './penguin-modal.scss';
 
 export const closeModalWindow = (modal) => {
-	modal.classList.remove('active');
-	modal.style.transform = 'translate(-50%, -50%) scale(0)';
-	window.setTimeout(() => (modal.style.zIndex = '-10000'), 500);
-	if (modal.classList.contains('custom-modal')) {
-		modal.remove();
-	}
+  modal.classList.remove('active');
+  modal.style.transform = 'translate(-50%, -50%) scale(0)';
+  window.setTimeout(() => (modal.style.zIndex = '-10000'), 500);
+  if (modal.classList.contains('custom-modal')) {
+    modal.remove();
+  }
 };
 
 export const closeModalWindowShadow = (modal) => {
-	closeModalWindow(modal);
-	const shadowDomElement = document.querySelector('.modal-window-shadow');
-	if (shadowDomElement) {
-		shadowDomElement.classList.remove('active');
-	}
-	document.querySelector('body').style.height = '';
-	document.querySelector('body').style.overflowY = '';
+  closeModalWindow(modal);
+  const shadowDomElement = document.querySelector('.modal-window-shadow');
+  if (shadowDomElement) {
+    shadowDomElement.classList.remove('active');
+  }
+  document.querySelector('body').style.height = '';
+  document.querySelector('body').style.overflowY = '';
 };
 
 export const addModalStyle = (
-	modal,
-	animationDuration = 500,
-	isMobile = false
+  modal,
+  animationDuration = 500,
+  isMobile = false
 ) => {
-	modal.style = `
+  modal.style = `
       transition: ${animationDuration}ms;
       display: block;
       position: fixed;
       will-change: transform;
       `;
 
-	const viewportHeight = window.innerHeight;
-	const modalHeight = modal.getBoundingClientRect().height;
+  const viewportHeight = window.innerHeight;
+  const modalHeight = modal.getBoundingClientRect().height;
 
-	modal.style.top = isMobile && modalHeight > viewportHeight ? '0' : '50%';
-	modal.style.left = '50%';
-	modal.style.transform = isMobile
-		? 'translateX(-50%) scale(0)'
-		: 'translate(-50%, -50%) scale(0)';
+  modal.style.top = isMobile && modalHeight > viewportHeight ? '0' : '50%';
+  modal.style.left = '50%';
+  modal.style.transform = isMobile
+    ? 'translateX(-50%) scale(0)'
+    : 'translate(-50%, -50%) scale(0)';
 };
 
 export const openModalWindow = (modal, isMobile = false, isFirst = true) => {
-	modal.classList.add('active');
-	modal.style.zIndex = '10000';
+  modal.classList.add('active');
+  modal.style.zIndex = '10000';
 
-	const viewportHeight = window.innerHeight;
-	const modalHeight = modal.getBoundingClientRect().height;
+  const viewportHeight = window.innerHeight;
+  const modalHeight = modal.getBoundingClientRect().height;
 
-	modal.style.transform =
-    isMobile && modalHeight > viewportHeight ? 'translateX(-50%) scale(1)' : 'translate(-50%, -50%) scale(1)';
-	document.querySelector('body').style.overflowY = 'hidden';
+  modal.style.transform =
+    isMobile && modalHeight > viewportHeight
+      ? 'translateX(-50%) scale(1)'
+      : 'translate(-50%, -50%) scale(1)';
+  document.querySelector('body').style.overflowY = 'hidden';
 
-	const modalCloseButtons = modal.querySelectorAll('[data-fancybox-close]');
-	
-	modalCloseButtons.forEach((modalCloseButton) => {
-		modalCloseButton.addEventListener('click', closeModal);
-	});
+  const modalCloseButtons = modal.querySelectorAll('[data-fancybox-close]');
 
-	if (isFirst) {
-		window.setTimeout(
-			() => document.body.addEventListener('click', onWindowClick),
-			500
-		);
-	}
+  modalCloseButtons.forEach((modalCloseButton) => {
+    modalCloseButton.addEventListener('click', closeModal);
+  });
 
-	const shadowElement = document.querySelector('.modal-window-shadow');
+  if (isFirst) {
+    window.setTimeout(
+      () => document.body.addEventListener('click', onWindowClick),
+      500
+    );
+  }
 
-	if (!document.querySelector('.modal-window-shadow')) {
-		createShadow();
-	}
+  if (!document.querySelector('.modal-window-shadow')) {
+    createShadow();
+  }
 
-	const shadowElement = document.querySelector('.modal-window-shadow');
-	shadowElement.classList.add('active');
+  const shadowElement = document.querySelector('.modal-window-shadow');
+  shadowElement.classList.add('active');
 
-	function onWindowClick(evt) {
-		if (!evt.target.closest('.c-modal')) {
-			closeModal();
-		}
-	}
+  function onWindowClick(evt) {
+    if (!evt.target.closest('.c-modal')) {
+      closeModal();
+    }
+  }
 
-	function closeModal() {
-		const activeModals = document.querySelectorAll('.c-modal.active');
+  function closeModal() {
+    const activeModals = document.querySelectorAll('.c-modal.active');
 
-		activeModals.forEach((modalEl) => {
-			closeModalWindowShadow(modalEl);
-		});
+    activeModals.forEach((modalEl) => {
+      closeModalWindowShadow(modalEl);
+    });
 
-		modalCloseButtons.forEach((modalCloseButton) => {
-			modalCloseButton.removeEventListener('click', closeModal);
-		});
-		document.body.removeEventListener('click', onWindowClick);
-	}
+    modalCloseButtons.forEach((modalCloseButton) => {
+      modalCloseButton.removeEventListener('click', closeModal);
+    });
+    document.body.removeEventListener('click', onWindowClick);
+  }
 };
 
 const createShadow = () => {
-	const shadowElement = document.createElement('div');
-	shadowElement.classList.add('modal-window-shadow');
-	document.body.appendChild(shadowElement);
-}
+  const shadowElement = document.createElement('div');
+  shadowElement.classList.add('modal-window-shadow');
+  document.body.appendChild(shadowElement);
+};
 
 export const startModals = () => {
-	const isMobile = window.matchMedia('(max-width: 1024px)').matches;
-	createShadow();
-	
-	const modalsButtons = document.querySelectorAll('.c-modal__button');
-	if (modalsButtons.length <= 0) {
-		return;
-	}
-	
-	modalsButtons.forEach((modalButton) => {
-		const modal = document.querySelector(modalButton.dataset.src);
-		addModalStyle(modal, 500, isMobile);
+  const isMobile = window.matchMedia('(max-width: 1024px)').matches;
+  createShadow();
 
-		const modalCloseButtons = modal.querySelectorAll('[data-fancybox-close]');
+  const modalsButtons = document.querySelectorAll('.c-modal__button');
+  if (modalsButtons.length <= 0) {
+    return;
+  }
 
-		if (modalCloseButtons.length <= 0) {
-			const closeModalButton = document.createElement('button');
-			closeModalButton.setAttribute('data-fancybox-close', '');
-			closeModalButton.setAttribute('type', 'button');
-			closeModalButton.innerHTML =
-									'<span class="visually-hidden">Кнопка закрытия модального окна</span>';
-			modal.appendChild(closeModalButton);
-		}
+  modalsButtons.forEach((modalButton) => {
+    const modal = document.querySelector(modalButton.dataset.src);
+    addModalStyle(modal, 500, isMobile);
 
-		modalButton.addEventListener('click', (evt) => {
-			evt.preventDefault();
-			const oldModal = document.querySelector('.c-modal.active');
-			if (oldModal) {
-				closeModalWindow(oldModal);
-			}
+    const modalCloseButtons = modal.querySelectorAll('[data-fancybox-close]');
 
-			openModalWindow(modal, isMobile);
-		});
-	});
+    if (modalCloseButtons.length <= 0) {
+      const closeModalButton = document.createElement('button');
+      closeModalButton.setAttribute('data-fancybox-close', '');
+      closeModalButton.setAttribute('type', 'button');
+      closeModalButton.innerHTML =
+        '<span class="visually-hidden">Кнопка закрытия модального окна</span>';
+      modal.appendChild(closeModalButton);
+    }
+
+    modalButton.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      const oldModal = document.querySelector('.c-modal.active');
+      if (oldModal) {
+        closeModalWindow(oldModal);
+      }
+
+      openModalWindow(modal, isMobile);
+    });
+  });
 };
