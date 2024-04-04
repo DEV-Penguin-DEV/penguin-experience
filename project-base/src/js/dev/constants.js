324e30c82b97f3d58a1748396b490f1dc541fe98
PENGUIN <kosvadmail@atomlc.com.ua>
2024-04-04 10:44:54 +0200

    Добавляет JS константы

---
+++ project-base/src/js/dev/constants.js
@@ -0,0 +1,12 @@
+// MEDIA
+export const MEDIA_REQUEST = {
+	minDesktopSize: window.matchMedia('(max-width: 1440px)'),
+	miniDesktopSize: window.matchMedia('(max-width: 1280px)'),
+	bigLaptopSize: window.matchMedia('(max-width: 1130px)'),
+	laptopSize: window.matchMedia('(max-width: 1000px)'),
+	bigTabletSize: window.matchMedia('(max-width: 900px)'),
+	tabletSize: window.matchMedia('(max-width: 760px)'),
+	miniTtabletSize: window.matchMedia('(max-width: 620px)'),
+	bigPhoneSize: window.matchMedia('(max-width: 500px)'),
+	phoneSize: window.matchMedia('(max-width: 440px)'),
+}; 
