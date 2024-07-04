bfb756371eb5f90ed2f7f7d7249f10453a5187ba
PENGUIN <kosvadmail@atomlc.com.ua>
2024-07-04 09:47:00 +0200

    Добавляет новую фукнцию обработки собития в плагин penguin-utils

--- penguin-utils.js
+++ penguin-utils.js
@@ -19,3 +19,20 @@ export const get = (selector, parentEl = document) => {
 export const getAll = (selector, parentEl = document) => {
 	return parentEl.querySelectorAll(selector);
 };
+
+/**
+ * Устанавливает обработчик события на контейнер и игнорирует все клики, кроме тех, которые соответствуют указанному селектору.
+ * 
+ * @param {Event} evt - Событие, вызванное кликом.
+ * @param {string} closestSelector - CSS селектор для поиска элемента, который должен обработать событие.
+ * @param {Function} cb - Колбэк функция, которая будет вызвана, если клик произошел на элементе, соответствующем селектору.
+ */
+export const clickEvent = (evt, closestSelector, cb) => {
+	try {
+        const target = evt.target;
+        if (!target.closest(closestSelector)) return;
+        cb(evt);
+    } catch (error) {
+        console.error('Invalid selector or error in clickEvent function:', error);
+    }
+}
